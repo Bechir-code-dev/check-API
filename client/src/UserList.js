31de8916e7e098d16cf832b9d0c9b7bf2ebe271a
bechir <bechir>
2025-03-16 16:26:56 +0100

    api

---
+++ client/src/UserList.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+import React , {useState,useEffect} from 'react';
+
+const UserList = () => {
+    const [] = useState([]);
+
+  useEffect(() => {
+    axios.get("")
+      .then()
+      .catch();
+  }, []);
+    return (
+        <>
+         
+        </>
+    )
+}
+
+export default UserList ;
