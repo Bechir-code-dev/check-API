4fe42f4b08071c3b1ae975fdecd0307e8f6ded76
bechir <bechir>
2025-03-17 22:29:08 +0100

    ppa

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
+import { useState } from 'react';
 import './App.css';
 import UserList from './UserList';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 function App() {
+  const [listofUser,setListofUser] = useState([]);
   return (
    <>
-   <UserList/>
+   <UserList setListofUser={setListofUser} listofUser={listofUser}/>
    </>
   );
 }
 
 export default App;
+// this the app.js 
--- client/src/UserList.js
+++ client/src/UserList.js
@@ -1,19 +1,50 @@
-import axios from 'axios';
-import React , {useState,useEffect} from 'react';
+import axios from "axios";
+import React, { useEffect } from "react";
+import Table from "react-bootstrap/Table";
 
-const UserList = () => {
-    const [] = useState([]);
+const UserList = ({ listofUser, setListofUser }) => {
+//use effect 
 
   useEffect(() => {
-    axios.get("")
-      .then()
-      .catch();
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((res) => setListofUser(res.data))
+      .catch((err) => console.error(err));
   }, []);
-    return (
-        <>
-         
-        </>
-    )
-}
+  return (
+    <div style={{padding:'20px', fontFamily:'sans-serif'}}>
+      <h2 style={{textDecoration:'underline' , display:'flex' , justifyContent:'center'}}>The total of user is {listofUser.length}</h2>
+      <Table striped bordered hover style={{border:'2px solid' , borderRadius:'5px'}}>
+        <thead>
+          <tr style={{textDecorationColor:'blue'}}>
+            <th>Id</th>
+            <th>Name</th>
+            <th>Username</th>
+            <th>Email</th>
+            <th>Address</th>
+            <th>Phone</th>
+            <th>Website</th>
+            <th>Company</th>
+          </tr>
+        </thead>
+        <tbody>
+          {listofUser.map(element => (
+            <tr>
+              <td>{element.id}</td>
+              <td>{element.name}</td>
+              <td>{element.username}</td>
+              <td>{element.email}</td>
+              <td>{element.address.street}</td>
+              <td>{element.phone}</td>
+              <td>{element.website}</td>
+              <td>{element.company.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
 
-export default UserList ;
+export default UserList;
+// user component 
